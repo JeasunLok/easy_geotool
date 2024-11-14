543c36b9f00d79aef2d7dd132fa6cc2723fff6bf
JeasunLok <1913587141@qq.com>
2024-11-14 22:48:46 +0800

    feat: add dev

--- examples/raster.py
+++ examples/raster.py
@@ -1,14 +1,19 @@
 import sys
-sys.path.append(r'E:\project\ljs\easy_geotool\easy_geotool')
+sys.path.append(r'D:\Workspace/project/easy_geotool/easy_geotool')
 
 from easy_geotool.io.raster import read_tif, write_tif
+from easy_geotool.transform.transform import raster_to_points, raster_to_lines, raster_to_polygons
 
-data_tif = r"E:\project\ljs\easy_geotool\easy_geotool\examples\data\gd_to_all.tif"
-example_tif = read_tif(data_tif)
-print(example_tif)
+input_path = r"D:\Workspace\project\easy_geotool\easy_geotool\examples\data\demo1.tif"
+output_path = r"D:\Workspace\project\easy_geotool\easy_geotool\examples\data\demo1.shp"
 
-example_tif_data = example_tif[0]
-im_geotrans, im_proj, cols, rows = example_tif[1], example_tif[2], example_tif[3], example_tif[4]
-write_tif(r"E:\project\ljs\easy_geotool\easy_geotool\examples\data\gd_to_all_write.tif", example_tif_data, im_geotrans, im_proj)
+# example_tif = read_tif(input_path)
+# print(example_tif)
 
+# example_tif_data = example_tif[0]
+# im_geotrans, im_proj, cols, rows = example_tif[1], example_tif[2], example_tif[3], example_tif[4]
+# write_tif(output_path, example_tif_data, im_geotrans, im_proj)
+
+
+raster_to_polygons(input_path, output_path, single_polygon=True)
 
--- examples/vector.py
+++ examples/vector.py
@@ -1,17 +1,17 @@
 import sys
-sys.path.append(r'E:\project\ljs\easy_geotool\easy_geotool')
+sys.path.append(r'D:\Workspace/project/easy_geotool/easy_geotool')
 
 from easy_geotool.io.vector import read_vector, write_vector
 from easy_geotool.transform.transform import vector_to_raster
 
 
-data_vector = r"E:\project\ljs\easy_geotool\easy_geotool\examples\data\net3_gd.shp"
-data_tif = r"E:\project\ljs\easy_geotool\easy_geotool\examples\data\net3_gd_raster.tif"
+data_vector = r"D:\Workspace/project/easy_geotool/easy_geotool/examples/data/chinapoint_project.shp"
+data_tif = r"D:\Workspace/project/easy_geotool/easy_geotool/examples/data/chinapoint_project.tif"
 
 # example_vector = read_vector(data_vector, format="shp")
 # print(example_vector)
 
 # write_vector(r"E:\project\ljs\easy_geotool\easy_geotool\examples\data\net3_gd.geojson", example_vector, "geojson")
 
-vector_to_raster(data_vector, data_tif, 1000, "field", 1, 1, "Id")
+vector_to_raster(data_vector, data_tif, 1000, "field", 1, 1)
 
